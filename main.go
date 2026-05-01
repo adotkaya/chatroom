b5c114cfec269acfa083c8317157a36f4d3db0d0
adotkaya <adotkaya@gmail.com>
2026-05-01 18:56:25 +0300

    upgrade as user connects

--- main.go
+++ main.go
@@ -40,7 +40,19 @@ func NewServer() *Server {
 	}
 }
 
-func handleWS(r http.ResponseWriter, w *http.Request) {
+func handleWS(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  512,
+		WriteBufferSize: 512,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 }
 
